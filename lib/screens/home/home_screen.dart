3e6d09678c12206a35f0e73ded0aa519dc827466
scott <qwe999993@gmail.com>
2025-04-25 17:24:11 +0800

    工作項目會有相應的圖示+點擊項目後會有小框顯示詳細訊息

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../add_screen.dart';
 
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -14,24 +13,34 @@ class _HomeScreenState extends State<HomeScreen> {
   // 模擬分組資料：以日期為 key，對應的工作紀錄為 value（List）
   final Map<String, List<Map<String, String>>> groupedRecords = {
     '111-06-10': [
-      {'crops' : '黑豆', 'task': '防病蟲害', 'field': 'D1~D3', 'note': ''},
-      {'crops' : '黑豆', 'task': '除草', 'field': 'D1~D3', 'note': '人工'},
-      {'crops' : '黑豆', 'task': '除草', 'field': 'D1~D3', 'note': '機器'},
+      {'crops': '黑豆', 'task': '防病蟲害', 'field': 'D1~D3', 'note': ''},
+      {'crops': '黑豆', 'task': '除草', 'field': 'D1~D3', 'note': '人工'},
+      {'crops': '黑豆', 'task': '除草', 'field': 'D1~D3', 'note': '機器'},
     ],
     '111-06-01': [
-      {'crops' : '黃豆', 'task': '施肥', 'field': 'D1~D3', 'note': ''},
-      {'crops' : '黃豆', 'task': '灌溉', 'field': 'D1~D6', 'note': ''},
+      {'crops': '黃豆', 'task': '施肥', 'field': 'D1~D3', 'note': ''},
+      {'crops': '黃豆', 'task': '灌溉', 'field': 'D1~D6', 'note': ''},
     ],
     '111-05-01': [
-      {'crops' : '黑豆', 'task': '播種', 'field': 'D1~D6', 'note': ''},
-      {'crops' : '黑豆', 'task': '整地', 'field': 'D1~D6', 'note': ''},
+      {'crops': '黑豆', 'task': '播種', 'field': 'D1~D6', 'note': ''},
+      {'crops': '黑豆', 'task': '整地', 'field': 'D1~D6', 'note': ''},
     ],
+  };
+
+  final Map<String, String> taskIcons = {
+    '播種': 'assets/seed.png',
+    '整地': 'assets/land.png',
+    '施肥': 'assets/muck.png',
+    '灌溉': 'assets/water.png',
+    '除草': 'assets/grass.png',
+    '防病蟲害': 'assets/worm.png',
   };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('農作物紀錄'),),
+      backgroundColor: Colors.green[100],
+      appBar: AppBar(title: const Text('農作物紀錄'), backgroundColor: Colors.green),
       body: ListView(
         children: [
           //標題
@@ -40,24 +49,45 @@ class _HomeScreenState extends State<HomeScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
             child: Row(
               children: const [
-                Expanded(flex: 2,
-                    child: Text(
-                        '日期', style: TextStyle(fontWeight: FontWeight.bold))),
-                Expanded(flex: 2,
-                    child: Text(
-                        '農作物', style: TextStyle(fontWeight: FontWeight.bold))),
-                Expanded(flex: 2,
-                    child: Text('工作項目',
-                        style: TextStyle(fontWeight: FontWeight.bold))),
-                Expanded(flex: 2,
-                    child: Text('田區代號',
-                        style: TextStyle(fontWeight: FontWeight.bold))),
-                Expanded(flex: 2,
-                    child: Text(
-                        '備註', style: TextStyle(fontWeight: FontWeight.bold))),
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    '日期',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    '農作物',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    '工作項目',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    '田區代號',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    '備註',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
               ],
             ),
           ),
+
           //分組資料
           ...groupedRecords.entries.map((entry) {
             return Column(
@@ -67,59 +97,102 @@ class _HomeScreenState extends State<HomeScreen> {
                 Container(
                   color: Colors.green[200],
                   padding: const EdgeInsets.symmetric(
-                      horizontal: 16, vertical: 8),
+                    horizontal: 16,
+                    vertical: 8,
+                  ),
                   child: Text(
                     entry.key,
                     style: const TextStyle(
-                        fontSize: 16, fontWeight: FontWeight.bold),
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
 
                 //每筆資料
                 ...entry.value.map((record) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 8, vertical: 4),
-                    child: Row(
-                      children: [
-                        // 空出日期欄位的寬度，讓對齊
-                        const Expanded(flex: 2, child: SizedBox()),
+                  return GestureDetector(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder:
+                            (context) => AlertDialog(
+                              title: const Text('詳細資訊'),
+                              content: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('日期：${entry.key}'),
+                                  Text('工作項目：${record['task']}'),
+                                  Text('田區代號：${record['field']}'),
+                                  Text(
+                                    '備註：${record['note']!.isEmpty ? '-' : record['note']}',
+                                  ),
+                                ],
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () => Navigator.pop(context),
+                                  child: const Text('關閉'),
+                                ),
+                              ],
+                            ),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 8,
+                        vertical: 4,
+                      ),
+                      child: Row(
+                        children: [
+                          // 空出日期欄位的寬度，讓對齊
+                          const Expanded(flex: 2, child: SizedBox()),
 
-                        //農作物
-                        Expanded(
+                          //農作物
+                          Expanded(flex: 2, child: Text(record['crops']!)),
+
+                          // 工作項目
+                          Expanded(
                             flex: 2,
-                            child: Text(record['crops']!)),
-
-                        // 工作項目
-                        Expanded(
-                          flex: 2,
-                          child: Row(
-                            children: [
-                              CircleAvatar(
-                                backgroundColor: Colors.green[100],
-                                radius: 16,
-                                child: const Icon(Icons.agriculture),
-                              ),
-                              const SizedBox(width: 8,),
-                              Text(
-                                record['task']!,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
+                            child: Row(
+                              children: [
+                                CircleAvatar(
+                                  backgroundColor: Colors.green[100],
+                                  radius: 30,
+                                  child: FittedBox(
+                                    child: Image.asset(
+                                      taskIcons[record['task']] ??
+                                          'assets/grass.png',
+                                      fit: BoxFit.contain,
+                                      width: 150,
+                                      height: 150,
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 8),
+                                Text(
+                                  record['task']!,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
 
-                        //田區代號
-                        Expanded(flex: 2, child: Text(record['field']!)),
+                          //田區代號
+                          Expanded(flex: 2, child: Text(record['field']!)),
 
-                        //備註
-                        Expanded(
-                          flex: 2,
-                          child: Text(
-                              record['note']!.isEmpty ? '-' : record['note']!),
-                        ),
-                      ],
+                          //備註
+                          Expanded(
+                            flex: 2,
+                            child: Text(
+                              record['note']!.isEmpty ? '-' : record['note']!,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   );
                 }).toList(),
@@ -134,16 +207,14 @@ class _HomeScreenState extends State<HomeScreen> {
         onPressed: () async {
           final newRecord = await Navigator.push(
             context,
-            MaterialPageRoute(
-                builder: (context) => const AddRecordScreen()
-            ),
+            MaterialPageRoute(builder: (context) => const AddRecordScreen()),
           );
-          if(newRecord != null){
+          if (newRecord != null) {
             final date = newRecord['date']!;
             setState(() {
               groupedRecords.putIfAbsent(date, () => []);
               groupedRecords[date]!.add({
-                'crops' : newRecord['crops']!,
+                'crops': newRecord['crops']!,
                 'task': newRecord['task']!,
                 'field': newRecord['field']!,
                 'note': newRecord['note']!,
@@ -152,11 +223,9 @@ class _HomeScreenState extends State<HomeScreen> {
           }
         },
         child: FittedBox(
-          child:
-          Image.asset('assets/pen.png', fit: BoxFit.contain,),
+          child: Image.asset('assets/pen.png', fit: BoxFit.contain),
         ),
       ),
-
     );
   }
 }
